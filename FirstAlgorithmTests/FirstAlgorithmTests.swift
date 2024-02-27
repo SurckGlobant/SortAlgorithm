c7e51fcabebdecde992e00d6ac59f186c15b1de1
surckarter <elsurck_fs@hotmail.com>
2024-02-27 17:09:59 -0600

    Unit tests will be added

--- FirstAlgorithmTests/FirstAlgorithmTests.swift
+++ FirstAlgorithmTests/FirstAlgorithmTests.swift
@@ -9,28 +9,23 @@ import XCTest
 @testable import FirstAlgorithm
 
 final class FirstAlgorithmTests: XCTestCase {
+  
+  private var sut: AlgorithmViewModel!
+  private var sortTextUseCase: SortTextUseCase!
+  
+  override func setUp() {
+    sortTextUseCase = SortTextUseCase()
+    sut = AlgorithmViewModel(useCases: AlgorithmViewModel.UseCases(sortText: sortTextUseCase))
+  }
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+  override func tearDown() {
+    sortTextUseCase = nil
+    sut = nil
+  }
+  
+  func testSortMethod(){
+    XCTAssertEqual("aceghjlnpqsuwyBDFIKMORTVXZ13590246®", sut.sortText(text: "aB4cD3eFg5®hIjKl2Mn1OpqR6sT9uVwX0yZ"))
+    XCTAssertEqual("abeCD4®", sut.sortText(text: "®DbeCa4"))
+    XCTAssertEqual("jjnW7024==", sut.sortText(text: "4jn70Wj==2"))
+  }
 }
